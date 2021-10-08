55b17b52d63f7456be66ab1c02ef34aa6eb64016
Osama <osamanadeem679@gmail.com>
2021-10-08 05:56:05 +0500

    styled orders table and toggle rating div

--- components/OrderTemp.js
+++ components/OrderTemp.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import styles from '../styles/OrderT.module.css'
+import { display } from "@mui/system";
+import React, { useState } from "react";
+import styles from "../styles/OrderTemp.module.css";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-function OrderTemp({data}) {
-    return (
-        <div onClick={()=>console.log(data.orders)} className={styles.root}>
-            <p>{data._id}</p>
-            <p>{data.userId}</p>
-            <p>{data.status}</p>
+function OrderTemp({ data }) {
+
+  const [flag, setFlag] = useState(0);
+
+  return (
+    <>
+        <div  className={styles.root}>
+            <div className={ styles.product__ImgName } >
+                <img
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdfestore.com%2Fwp-content%2Fuploads%2F2020%2F10%2F10348-1.jpg&f=1&nofb=1"
+                alt="rgb keyboard"
+                style={{ height: '100px', width: '100px', objectFit: 'contain' }}
+                />
+                <p>{ `${data.orders[0].name} ...` }</p>
+            </div>
+            <div className={ styles.orderDetails } >
+                <p>{data._id}</p>
+                <p>{data.userId}</p>
+                <p>{data.status}</p>
+                <KeyboardArrowDownIcon className={ styles.dropIcon } onClick={() => flag ? setFlag(0) : setFlag(1)} />
+            </div>
         </div>
-    )
+        <div 
+        style={
+            flag ? { display: 'flex', backgroundColor: 'lightblue', width: '900px', height: '300px', border: '2px solid white' } 
+            : 
+            { display: 'none' }} 
+        >
+        hehehe
+        </div>
+
+
+    </>
+  );
 }
 
-export default OrderTemp
+export default OrderTemp;
--- pages/thankyou.js
+++ pages/thankyou.js
@@ -1,7 +1,13 @@
 import { useStoreActions } from "easy-peasy";
+import { useRouter } from 'next/router'
 
 function thankyou () {
     
+    const router = useRouter();
+    setTimeout(() => {
+        router.push('/myOrders');
+    }, 3000);
+
     return (
         <div style={{height:'100vh',overflow:'auto'}}>
             <h1>Thankyou for your patience with our site!</h1>
