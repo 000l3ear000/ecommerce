927e6e2b74ff52bcce4c8ac5760dbe7fd2340afa
000l3ear000 <ksaad608@gmail.com>
2021-10-11 03:05:38 +0500

    styled payment option page

--- components/UserShippingAddress.js
+++ components/UserShippingAddress.js
@@ -8,14 +8,15 @@ function UserShippingAddress({subtotal, user, execOrder }) {
 
     return (
         <div className={styles.root}>
-            <h4>First Name: { user.firstName }</h4>
-            <h4>Last Name: { user.lastName }</h4>
-            <h4>Email: { user.email }</h4>
-            <h4>Address: { user.address }</h4>
-            <h4>City: { user.city }</h4>
-            <h1>{subtotal}</h1>
-            <button onClick={() => router.push('/cart')} >EDIT KARNA ABHI</button>
-            <button onClick={(e) => execOrder(e) } >AAGE CHALO</button>
+            <h2 style={{border:'1px solid white',textAlign:'center',padding:'5px'}}>Shipping Details</h2>
+            <h4 className={styles.hello}>First Name: { user.firstName }</h4>
+            <h4 className={styles.hello}>Last Name: { user.lastName }</h4>
+            <h4 className={styles.hello}>Email: { user.email }</h4>
+            <h4 className={styles.hello}>Address: { user.address }</h4>
+            <h4 className={styles.hello}> City: { user.city }</h4>
+            <h1 style={{textAlign:'center'}}>Total:- {subtotal}</h1>
+            <button style={{height:'40px'}} onClick={() => router.push('/cart')} >Edit Info</button>
+            
         </div>
     )
 }
--- pages/payment.js
+++ pages/payment.js
@@ -2,11 +2,11 @@ import { useRouter } from 'next/dist/client/router'
 import React, { useEffect, useState } from 'react'
 import cookies from 'js-cookies'
 import styles from '../styles/Payment.module.css'
-import  OrderedProducts  from '../components/OrderedProducts';
-import  UserShippingAddress  from '../components/UserShippingAddress';
-import {useStoreActions} from 'easy-peasy'
+import OrderedProducts from '../components/OrderedProducts';
+import UserShippingAddress from '../components/UserShippingAddress';
+import { useStoreActions } from 'easy-peasy'
 import { baseUrl } from '../constants/baseUrl';
-import { CircularProgress,Box } from '@mui/material';
+import { CircularProgress, Box } from '@mui/material';
 
 
 function payment() {
@@ -17,133 +17,133 @@ function payment() {
     const [user, setUser] = useState({});
     const [subtotal, setSubtotal] = useState(0);
     const [paymentMethod, setPaymentMethod] = useState("");
-    const settoggle=useStoreActions((actions)=>actions.settoggle)
+    const settoggle = useStoreActions((actions) => actions.settoggle)
     const [emailSent, setEmailSent] = useState('');
 
     useEffect(() => {
-        const arr=[]
-        const data=JSON.parse(localStorage.getItem("cartItems"))
+        const arr = []
+        const data = JSON.parse(localStorage.getItem("cartItems"))
         console.log("THESE ARE CART ITEMS >>>> ", data);
-        setOrderedProducts( data );
+        setOrderedProducts(data);
 
-        if(data){
+        if (data) {
             data.forEach(element => {
-                arr.push({id:element._id,qty:element.cartQuantity})
+                arr.push({ id: element._id, qty: element.cartQuantity })
             });
         }
-        else{
+        else {
             router.push('/cart')
         }
-        console.log("This is the final order list",arr)
+        console.log("This is the final order list", arr)
         setstate(arr);
     }, [])
 
     useEffect(() => {
-        if ( paymentMethod === "cash_on_delivery" ){
+        if (paymentMethod === "cash_on_delivery") {
             // push forward
             console.log("CASH ON DELIVERY", paymentMethod);
         }
-        else if ( paymentMethod === "card_payment" ){
+        else if (paymentMethod === "card_payment") {
             // card module
             console.log("CARD PAYMENT")
         }
-        else{
+        else {
             console.log('SOME OTHER ERROR OCCURED')
         }
     }, [paymentMethod])
 
     const getSubtotal = () => {
         let tempSubtotal = 0;
-        orderedProducts.forEach( item => {
+        orderedProducts.forEach(item => {
             tempSubtotal += item.cartQuantity * item.price;
-        } )
-        setSubtotal( tempSubtotal );
+        })
+        setSubtotal(tempSubtotal);
     }
 
     useEffect(() => {
         getSubtotal();
-    },[orderedProducts])
+    }, [orderedProducts])
 
     useEffect(() => {
         const shippingAddress = async () => {
             const response = await fetch("http://localhost:3000/api/getUserShippingAddress?userid=" + cookies.getItem("id"))
             const userShippingAddress = await response.json();
             var data = ""
-            if ( userShippingAddress.success && userShippingAddress.address){
-                setUser( userShippingAddress.address )
+            if (userShippingAddress.success && userShippingAddress.address) {
+                setUser(userShippingAddress.address)
             }
-            else{
+            else {
                 console.log("New User")
             }
             // document.getElementById('sad').value=data;
 
         }
-        if(cookies.getItem('id')){
+        if (cookies.getItem('id')) {
             shippingAddress();
         }
-        else{
+        else {
             router.push('/auth/login?onCart=true')
         }
     }, [])
-    
-    const placeOrder = async(e) => {
-        e.target.disabled=true
+
+    const placeOrder = async (e) => {
+        e.target.disabled = true
         const res = await fetch("http://localhost:3000/api/fetchPaymentProducts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "userId": cookies.getItem('id'),
+                "_jwt": cookies.getItem("jwt")
+            },
+            body: JSON.stringify(state)
+        })
+        // rest logic here
+        const checkOrderInsertion = await res.json();
+        if (checkOrderInsertion.success) {
+            console.log("YAAAAYYYY WE GOT YOUR PAYMENT");
+            localStorage.clear()
+            settoggle()
+            setFlag(1);
+            if (cookies.getItem('jwt')) {
+                const response = await fetch(`${baseUrl}/api/emailVerification`, {
                     method: "POST",
                     headers: {
-                        "Content-Type": "application/json",                    
-                        "userId": cookies.getItem('id'),
-                        "_jwt": cookies.getItem("jwt")
+                        "Content-Type": "application/json",
+                        "email": cookies.getItem('email'),
+                        "token": cookies.getItem('jwt'),
+                        "action": 'order confirm',
                     },
-                    body: JSON.stringify(state)
-                })
-                // rest logic here
-                const checkOrderInsertion = await res.json();
-                if ( checkOrderInsertion.success ){
-                    console.log("YAAAAYYYY WE GOT YOUR PAYMENT");
-                    localStorage.clear()
-                    settoggle()
-                    setFlag(1);
-                    if( cookies.getItem('jwt') ){
-                        const response = await fetch(`${baseUrl}/api/emailVerification`, {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",                    
-                                "email": cookies.getItem('email'),
-                                "token": cookies.getItem('jwt'),
-                                "action": 'order confirm',
-                            },
-                        });
-                        const checkEmail = await response.json();
-                        if ( checkEmail.err === "hello" ){
-                            setEmailSent("Email sent for your order confirmation")
-                            setTimeout(()=>{
-                                router.push('/thankyou')
-                            },2000)
-                        }
-                        else{
-                            setEmailSent("Could Not Send Email",checkEmail.err)
-                        }
-                    }
-                    
-                    // setOrderedProducts( checkOrderInsertion.order.orders );
-                    // setUser( checkOrderInsertion.user );
+                });
+                const checkEmail = await response.json();
+                if (checkEmail.err === "hello") {
+                    setEmailSent("Email sent for your order confirmation")
+                    setTimeout(() => {
+                        router.push('/thankyou')
+                    }, 2000)
+                }
+                else {
+                    setEmailSent("Could Not Send Email", checkEmail.err)
+                }
+            }
 
-                }
-                else if(checkOrderInsertion){
-                    console.log("ERRRRRRRRROOOOOOOOOOORRRRRRRRRRR <<<<<<<<< ", checkOrderInsertion);
-                    cookies.setItem("jwt", "");
-                    cookies.setItem("name", "");
-                    cookies.setItem("email", "");
-                    cookies.setItem("id", "");
-                    cookies.setItem("address", "");
-                    router.push('/auth/login?onPayment=true')
-                }
+            // setOrderedProducts( checkOrderInsertion.order.orders );
+            // setUser( checkOrderInsertion.user );
+
+        }
+        else if (checkOrderInsertion) {
+            console.log("ERRRRRRRRROOOOOOOOOOORRRRRRRRRRR <<<<<<<<< ", checkOrderInsertion);
+            cookies.setItem("jwt", "");
+            cookies.setItem("name", "");
+            cookies.setItem("email", "");
+            cookies.setItem("id", "");
+            cookies.setItem("address", "");
+            router.push('/auth/login?onPayment=true')
+        }
     }
-    
-    const handleChange =  (e) => {
 
-        setPaymentMethod( e.target.value );
+    const handleChange = (e) => {
+
+        setPaymentMethod(e.target.value);
 
     };
     // <div className={ styles.orders__subtotal } ></div>
@@ -152,36 +152,41 @@ function payment() {
     return (
         <div className={styles.root}>
             <div className={styles.first}>
-                { orderedProducts?.map(product => (
-                    <OrderedProducts key={ product._id } product={ product } />
-                )) }
+                {orderedProducts?.map(product => (
+                    <OrderedProducts key={product._id} product={product} />
+                ))}
                 <h1>{subtotal}</h1>
             </div>
-            
-            { user? <UserShippingAddress subtotal={subtotal} user={ user } execOrder={placeOrder} /> : null }
+
+            {user ? <UserShippingAddress subtotal={subtotal} user={user} execOrder={placeOrder} /> : null}
             {
-                subtotal? (
+                subtotal ? (
                     <div className="payment__method" >
-                    {
-                        (
-                            <div style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
-                                <div style={{display:'flex',justifyContent:'center'}}>
-                                    <input onChange={ (e) => handleChange(e)} disabled={subtotal>5000?true:false} type="radio" name="payment" value="cash_on_delivery"/><span style={subtotal>5000?{textDecoration:"line-through"}:null}>Cash on delivery</span>
-                                    <input onChange={ (e) => handleChange(e)} type="radio" name="payment" value="card_payment" /><span>Card payment</span>
+                        {
+                            (
+                                <div style={{ display: 'flex', color: 'white', fontFamily: 'fantasy', border: '1px solid black', fontFamily: 'sans-serif', backgroundColor: '#F96302', padding: '10px', flexDirection: 'column', alignItems: 'center' }}>
+                                    <h2 style={{ border: '1px solid white', marginBottom: '80px', padding: '8px' }}>Select A Payment Method</h2>
+                                    <div style={{ display: 'flex', color: 'white',flexDirection:"column",marginBottom:'20px', justifyContent: 'center' }}>
+                                        <div className={styles.select}>
+                                            <input onChange={(e) => handleChange(e)} disabled={subtotal > 5000 ? true : false} type="radio" name="payment" value="cash_on_delivery" /><span style={subtotal > 5000 ? { textDecoration: "line-through" } : null}>Cash on delivery</span>
+                                        </div>
+                                        <div className={styles.select}>
+                                            <input onChange={(e) => handleChange(e)} type="radio" name="payment" value="card_payment" /><span>Card payment</span>
+                                        </div>
+                                    </div>
+                                    <h5 style={subtotal > 5000 ? { color: 'white' } : { display: "none" }}>Sorry you cannot use Cash On Delivery on amount greater than 5000 rupees</h5>
+                                    {
+                                        flag ? (
+                                            <Box sx={{ display: 'flex' }}>
+                                                <CircularProgress />
+                                            </Box>
+                                        ) : (
+                                            <button disabled={!paymentMethod} style={{ height: '35px', borderRadius: '5px', border: '1px solid white', backgroundColor: "#F96302" }} onClick={(e) => placeOrder(e)}>PlaceOrder</button>
+                                        )
+                                    }
                                 </div>
-                                <h5 style={subtotal>5000?null:{display:"none"}}>Sorry you cannot use Cash On Delivery on amount greater than 5000 rupees</h5>
-                                {
-                                    flag ? (
-                                        <Box sx={{ display: 'flex' }}>
-                                            <CircularProgress />
-                                        </Box>
-                                    ) : (
-                                        <button disabled={!paymentMethod} onClick={(e)=>placeOrder(e)}>PlaceOrder</button>
-                                    )
-                                }
-                            </div>
-                        ) 
-                    }
+                            )
+                        }
                     </div>
                 ) : (
                     <h5>Subtotal isnt calculated yet</h5>
